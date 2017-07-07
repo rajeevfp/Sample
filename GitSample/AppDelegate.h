7c4e23bddf74fd175306ab07748166b9038f2b50
rajeevfp <rajeevpulleti@Rajeevs-MacBook-Pro.local>
2017-07-07 10:16:19 +0530

    firstCommit

---
+++ GitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSample
+//
+//  Created by Rajeev Pulleti on 07/07/17.
+//  Copyright © 2017 Rajeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by Rajeev Pulleti on 07/07/17.
+//  Copyright © 2017 Rajeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
